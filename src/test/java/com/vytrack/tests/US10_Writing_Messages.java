b5a0f2c8e582e9b635426b65603994543665d214
tnas0 <tubanas32@hotmail.com>
2022-03-10 13:26:28 -0800

    user go to Activities tab and hover over

--- src/test/java/com/vytrack/tests/US10_Writing_Messages.java
+++ src/test/java/com/vytrack/tests/US10_Writing_Messages.java
@@ -1,7 +1,12 @@
 package com.vytrack.tests;
 
 import com.vytrack.tests.base.TestBase;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class US10_Writing_Messages extends TestBase {
@@ -10,6 +15,11 @@ public class US10_Writing_Messages extends TestBase {
 
       //step1-user go to homepage as sale manager
       VytrackUtils.loginAsSalesManager();
-
+     //step2- user go to Activities Tab and hover over
+      String ActivitiesElementLocator="//span[normalize-space()='Activities' and contains (@class,'title title-level-1')]";
+     WebElement ActivitiesElement= Driver.getDriver().findElement(By.xpath(ActivitiesElementLocator));
+      Actions actions=new Actions(Driver.getDriver());
+      actions.moveToElement(ActivitiesElement).perform();
+      BrowserUtils.sleep(3);
    }
 }
