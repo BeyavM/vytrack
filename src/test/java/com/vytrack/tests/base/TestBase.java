64540087a8823309f791796b5bca2db13e395cef
Omar <Oumerahmed0@gmail.com>
2022-03-13 18:40:07 -0700

    initial commit 2

--- src/test/java/com/vytrack/tests/base/TestBase.java
+++ src/test/java/com/vytrack/tests/base/TestBase.java
@@ -2,15 +2,21 @@ package com.vytrack.tests.base;
 
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public abstract class TestBase {
+
     @BeforeMethod
     public void setUp() {
         Driver.getDriver().get(ConfigurationReader.getProperty("env"));
     }
+
+
+
 
     @AfterMethod
     public void tearDown() {
